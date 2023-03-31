4956373f6d8d8411cf45fe34d9240707104a41f0
illyabezpaliy13 <110487623+illyabezpaliy13@users.noreply.github.com>
2023-03-31 12:07:39 +0300

    доробив

--- js/index10.js
+++ js/index10.js
@@ -2,8 +2,8 @@ let a = 5;
 let b = 10;
 let c = 15;
 let d = 20;
-a = a + 2;
-b = b - 4;
-c = c * 3;
-d = d / 10;
+a += 2;
+b -= 4
+c *= 3
+d /= 10
 console.log(a, b, c, d);
--- js/index7.js
+++ js/index7.js
@@ -1,6 +1,6 @@
 const pricePerDroid = "800";
-const orderedQuantity = "6"
-const deliveryFee = "50"
+const orderedQuantity = "6";
+const deliveryFee = "50";
 const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
-const message = `You ordered droids worth ${totalPrice} credits. Delivery (${50} credits) is included in total price.`
-console.log(message)
+const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee}} credits) is included in total price.`;
+console.log(message);
